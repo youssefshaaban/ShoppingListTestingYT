d1956e7e275f841883cb2713bb345c75e3245a11
yousef <yousefshaaban0@gmail.com>
2022-04-16 15:13:25 +0200

    finish using hilt for injection inside test

--- app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/data/local/ShoppingDaTest.kt
+++ app/src/androidTest/java/com/androiddevs/shoppinglisttestingyt/data/local/ShoppingDaTest.kt
@@ -8,68 +8,79 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.androiddevs.shoppinglisttestingyt.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
-@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
 @SmallTest
+@HiltAndroidTest
 class ShoppingDaTest {
-    private lateinit var shoppingItemDatabase: ShoppingItemDatabase
 
-    private lateinit var shoppingDoa: ShoppingDoa
+    @Inject
+    @Named("test_db")
+    lateinit var shoppingItemDatabase: ShoppingItemDatabase
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var shoppingDoa: ShoppingDoa
+
     @Before
     fun setup() {
-        shoppingItemDatabase = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            ShoppingItemDatabase::class.java
-        ).allowMainThreadQueries().build()
-        shoppingDoa=shoppingItemDatabase.shoppingDao()
+        hiltRule.inject()
+        shoppingDoa = shoppingItemDatabase.shoppingDao()
     }
 
     @After
-    fun tearDown(){
+    fun tearDown() {
         shoppingItemDatabase.close()
     }
 
     @Test
-    fun insertShopping(){
+    fun insertShopping() {
         runBlockingTest {
-            val shoppingItem=ShoppingItem("mmm",3,34.5f,"url", id = 1)
+            val shoppingItem = ShoppingItem("mmm", 3, 34.5f, "url", id = 1)
             shoppingDoa.insertShoppingItem(shoppingItem)
-            val allShoppingItem=shoppingDoa.observeAllShoppingItems().getOrAwaitValue()
+            val allShoppingItem = shoppingDoa.observeAllShoppingItems().getOrAwaitValue()
             assertThat(allShoppingItem).contains(shoppingItem)
         }
     }
 
     @Test
-    fun deleteShoppingItem(){
+    fun deleteShoppingItem() {
         runBlockingTest {
-            val shoppingItem=ShoppingItem("mmm",3,34.5f,"url", id = 1)
+            val shoppingItem = ShoppingItem("mmm", 3, 34.5f, "url", id = 1)
             shoppingDoa.insertShoppingItem(shoppingItem)
             shoppingDoa.deleteShoppingItem(shoppingItem)
-            val allShoppingItem=shoppingDoa.observeAllShoppingItems().getOrAwaitValue()
+            val allShoppingItem = shoppingDoa.observeAllShoppingItems().getOrAwaitValue()
             assertThat(allShoppingItem).doesNotContain(shoppingItem)
         }
     }
 
     @Test
-    fun observeTotalPrice(){
+    fun observeTotalPrice() {
         runBlockingTest {
-            val shoppingItem=ShoppingItem("mmm",3,34.5f,"url", id = 1)
-            val shoppingItem1=ShoppingItem("mmm",2,34.5f,"url", id = 2)
-            val shoppingItem2=ShoppingItem("mmm",1,34.5f,"url", id = 3)
+            val shoppingItem = ShoppingItem("mmm", 3, 34.5f, "url", id = 1)
+            val shoppingItem1 = ShoppingItem("mmm", 2, 34.5f, "url", id = 2)
+            val shoppingItem2 = ShoppingItem("mmm", 1, 34.5f, "url", id = 3)
             shoppingDoa.insertShoppingItem(shoppingItem)
             shoppingDoa.insertShoppingItem(shoppingItem1)
             shoppingDoa.insertShoppingItem(shoppingItem2)
 
-            val totalPrice=shoppingDoa.observeTotalPrice().getOrAwaitValue()
-            assertThat(totalPrice).isEqualTo(3*34.5f + 2*34.5f +34.5f)
+            val totalPrice = shoppingDoa.observeTotalPrice().getOrAwaitValue()
+            assertThat(totalPrice).isEqualTo(3 * 34.5f + 2 * 34.5f + 34.5f)
         }
     }
 }
